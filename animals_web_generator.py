14177bb698ceb45a39679a983a8bafe2ed27f085
Sinan <sinankahraman@hotmail.de>
2026-02-12 13:57:27 +0100

    Improve animal card details layout

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,46 +14,41 @@ def load_website(html_path: str) -> str:
 
 
 def serialize_animal(animal: dict) -> str:
-    """Serialize a single animal dict into an extended HTML card."""
+    """Serialize a single animal dict into structured HTML."""
     name = animal.get("name")
 
     locations = animal.get("locations", [])
     location = locations[0] if locations else None
-
-    taxonomy = animal.get("taxonomy", {})
-    scientific_name = taxonomy.get("scientific_name")
 
     characteristics = animal.get("characteristics", {})
     diet = characteristics.get("diet")
     animal_type = characteristics.get("type")
     lifespan = characteristics.get("lifespan")
     top_speed = characteristics.get("top_speed")
-    weight = characteristics.get("weight")
-    habitat = characteristics.get("habitat")
-    color = characteristics.get("color")
 
     def field(label, value):
-        return f"<strong>{label}:</strong> {value}<br/>" if value else ""
+        if value:
+            return f'<li class="card__detail"><strong>{label}:</strong> {value}</li>'
+        return ""
 
     output = ""
     output += '<li class="cards__item">'
     output += f'<div class="card__title">{name}</div>'
-    output += '<p class="card__text">'
+    output += '<div class="card__text">'
+    output += '<ul class="card__details">'
 
-    output += field("Scientific Name", scientific_name)
     output += field("Diet", diet)
     output += field("Location", location)
     output += field("Type", animal_type)
-    output += field("Habitat", habitat)
-    output += field("Top Speed", top_speed)
-    output += field("Weight", weight)
     output += field("Lifespan", lifespan)
-    output += field("Color", color)
+    output += field("Top Speed", top_speed)
 
-    output += "</p>"
+    output += "</ul>"
+    output += "</div>"
     output += "</li>"
 
     return output
+
 
 
 
